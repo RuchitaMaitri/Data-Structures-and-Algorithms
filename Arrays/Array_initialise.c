5bf0ac6d0266ba547c8b1e4e4bff6a3f01802403
RuchitaMaitri <ruchitamaitri09@gmail.com>
2020-12-02 12:30:00 +0530

    Initialise array structure

---
+++ Arrays/Array_initialise.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Array {
+    int *A;
+    int size;
+    int length;
+};
+
+int main(){
+    struct Array arr;
+    printf("Enter the size of array:");
+    scanf("%d", &arr.size);
+    arr.A = (int *)malloc(sizeof(int) * arr.size);
+    arr.length = 0;
+}
