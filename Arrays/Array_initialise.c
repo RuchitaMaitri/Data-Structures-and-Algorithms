35abef29c0c6792d55e68ac9b4bf00ee23db6d91
RuchitaMaitri <ruchitamaitri09@gmail.com>
2020-12-02 13:14:20 +0530

    Display function added

--- Arrays/Array_initialise.c
+++ Arrays/Array_initialise.c
@@ -7,11 +7,29 @@ struct Array {
     int size;
     int length;
 };
-
+void Display(struct Array A){
+    int i;
+    printf("Elements of array are: \n");
+    for(i=0; i < A.length; i++){
+        printf("%d ", A.A[i]);
+    }
+}
 int main(){
     struct Array arr;
-    printf("Enter the size of array:");
+    printf("Enter the size of array:\n");
     scanf("%d", &arr.size);
     arr.A = (int *)malloc(sizeof(int) * arr.size);
     arr.length = 0;
+
+    int n;
+    printf("Enter the number of numbers:\n");
+    scanf("%d",&n);
+    printf("Enter all elements: \n");
+
+    int i;
+    for(i = 0; i < n; i++){
+        scanf("%d",&arr.A[i]);
+    }
+   arr.length = n;
+   Display(arr);
 }
