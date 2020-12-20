1becbad39430384ae6268faa792b87526d399bf8
RuchitaMaitri <ruchitamaitri09@gmail.com>
2020-12-20 20:13:39 +0530

    Update sum and count function for Linked List

--- LinkedList/DisplayLL.c
+++ LinkedList/DisplayLL.c
@@ -22,17 +22,71 @@ void create(int A[], int n){
         last = t; 
     }
 }
-
+//Iterative
 void Display(struct Node *p){
     while(p!= NULL){
         printf("%d ", p-> data);
         p = p->next;
     }
-
+    printf("\n");
+}
+//Recursive
+void RDisplay(struct Node *p){
+    if(p!=NULL){
+        printf("%d ", p->data);
+        RDisplay(p->next);
+    }
+    printf("\n");
+}
+//Print in reverse - Recursive
+void RDisplayRecursive(struct Node *p){
+    if(p!=NULL){
+        RDisplayRecursive(p->next);
+        printf("%d ", p->data);
+    }
+    printf("\n");
+}
+//finding length of linked list - Iterative
+int Count(struct Node *p){
+    int l = 0;
+    while(p != NULL){
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+//finding length of linked list - Recursive version
+int CountR(struct Node *p){
+    if(p != NULL){ 
+        return Count(p->next) + 1;
+    } else {
+        return 0;
+    }
+}
+//finding sum of linked list - Iterative
+int Sum(struct Node *p){
+    int s = 0;
+    while(p != NULL){
+        s = s + p->data;
+        p = p->next;
+    }
+    return s;
 }
 
+//finding sum of linked list - Recursive version
+int SumR(struct Node *p){
+    if(p != NULL){ 
+        return SumR(p->next) + p->data;
+    } else {
+        return 0;
+    }
+}
 int main(){
-    int A[] = {3,5,7,10,15};
-    create(A, 5);
+    int A[] = {3,5,7,10,15,8,12,20};
+    create(A, 8);
     Display(first);
+    RDisplay(first);
+    RDisplayRecursive(first);
+    printf("length is %d \n", CountR(first));
+    printf("Sum is %d \n", SumR(first));
 }
