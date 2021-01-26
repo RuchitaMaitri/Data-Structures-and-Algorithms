d56e554c8ef506b74479c7c0c7b04bc7e51effd0
RuchitaMaitri <ruchitamaitri09@gmail.com>
2021-01-26 14:58:05 +0530

    computes height and counts no of nodes of tree

---
+++ Trees/HeighCount.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+};
+
+struct Queue {
+    int size;
+    int front;
+    int rear;
+    struct Node  **Q;
+};
+
+void create(struct Queue *q, int size){
+    q->size=size;
+    q->front=q->rear=0;
+    q->Q=(struct Node **)malloc(q->size*sizeof(struct Node *));
+}
+
+void enqueue(struct Queue *q, struct Node *x){
+    if((q->rear+1)%q->size==q->front){
+        printf("Queue is full");
+    }
+    else {
+            q->rear=(q->rear+1)%q->size;
+            q->Q[q->rear]=x;
+        }
+}
+struct Node * dequeue(struct Queue *q){
+    struct Node * x=NULL;
+    if(q->front==q->rear){
+        printf("Queue is empty");
+    }else{
+        q->front=(q->front+1)%q->size;
+        x=q->Q[q->front];
+    }
+    return x;
+}
+
+int isEmpty(struct Queue q){
+    return q.front==q.rear;
+}
+struct Node *root=NULL;
+
+void createTree(){
+    struct Node *p, *t;
+    int x;
+    struct Queue q;
+    create(&q,100);
+
+    root=(struct Node *)malloc(sizeof(struct Node));
+    printf("Enter root value");
+    scanf("%d", &x);
+    root->data=x;
+    root->lchild=root->rchild=NULL;
+    enqueue(&q, root);
+
+    while(!isEmpty(q)){
+
+        p=dequeue(&q);
+
+        printf("Enter left child of %d", p->data);
+        scanf("%d", &x);
+        if(x!=-1){
+            t=(struct Node *)malloc(sizeof(struct Node));
+            t->data=x;
+            t->lchild=t->rchild=NULL;
+            p->lchild=t;
+            enqueue(&q,t);
+        }
+        printf("Enter right child of %d", p->data);
+        scanf("%d", &x);
+        if(x!=-1){
+            t=(struct Node *)malloc(sizeof(struct Node));
+            t->data=x;
+            t->lchild=t->rchild=NULL;
+            p->rchild=t;
+            enqueue(&q,t);
+        }
+
+    }
+
+}
+
+int count(struct Node *p){
+    if(p==NULL) 
+        return 0;
+    return count(p->lchild) + count(p->rchild) + 1;
+}
+
+int height(struct Node *p){
+    int x=0, y=0;
+    if(p){
+        x = height(p->lchild);
+        y = height(p->rchild);
+        if(x>y) return x+1;
+        else return y+1;
+    }
+    return 0;
+}
+int main(){
+
+    createTree();
+    printf("count of nodes = %d ", count(root));
+    printf("height of tree = %d ", height(root)-1);
+}
