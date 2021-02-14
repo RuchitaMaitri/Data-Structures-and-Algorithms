ee0ef9c34712356745609b1b6413abca8d35cef1
RuchitaMaitri <ruchitamaitri09@gmail.com>
2021-02-14 22:19:57 +0530

    perform selection sort

--- Sorts/Sorting.c
+++ Sorts/Sorting.c
@@ -31,11 +31,23 @@ void Insertion(int A[], int n){
         A[j+1]=x;
     }
 }
+void SelectionSort(int A[], int n){
+    int i,j,k;
+    for(i=0;i<n-1;i++){
+        for(j=k=i;j<n;j++){
+            if(A[j]<A[k]){
+                k=j;
+            }
+        }
+        swap(&A[i],&A[k]);
+    }
+}
 int main(){
     int A[]={3,7,9,10,6,5,12,4,11,2};
     int n=10;
     // Bubble(A,10);
-    Insertion(A,10);
+    // Insertion(A,10);
+    SelectionSort(A,10);
     int i;
     for(i=0;i<10;i++){
         printf("%d ", A[i]);
